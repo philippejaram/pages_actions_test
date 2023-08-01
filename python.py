1cefa45504769dbe06c982cbc9c22f914d2a307e
Philippe Jara. Martins <philippe.jara@leblonequities.com.br>
2023-08-01 09:56:31 -0300

    .

--- python.py
+++ python.py
@@ -2,3 +2,23 @@ def main():
     print("test")
     print("2")
     return 0
+
+
+def f2(strarg, floatarg):
+    """
+    Description of the function
+
+    
+    Parameters
+    --------------
+    strarg : srt
+       str argument
+    floatarg : float
+       float argument
+
+    Returns
+    -------------
+    str
+       str argument
+    """
+    return (strarg)
